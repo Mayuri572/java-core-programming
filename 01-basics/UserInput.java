632a35be4d495afe798eb61d8bbb199fb4640ce3
Mayuri Jadhav <jmayuri572@gmail.com>
2026-02-07 23:46:02 +0530

    feat: completed java basics (variables, data types, user input)

--- 01-basics/UserInput.java
+++ 01-basics/UserInput.java
@@ -1,10 +1,9 @@
 import java.util.Scanner;
 public class UserInput {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter your name:");
-        String name = sc.nextLine();
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter your name:");
+            String name = sc.nextLine();
 
         System.out.println("Enter your age:");
         int age = sc.nextInt();
@@ -21,4 +20,5 @@ public class UserInput {
 
       }
 
+}
 }
