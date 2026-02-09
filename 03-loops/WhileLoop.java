9e44616d55c75829259ae31d82df89b787f2bf67
Mayuri <jmayuri572@gmail.com>
2026-02-09 23:09:13 +0530

    feat: added for and while loop examples

---
+++ 03-loops/WhileLoop.java
@@ -0,0 +1,9 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        int count = 1;
+        while (count <= 5) {
+            System.out.println("Hello, World! " + count);
+            count++;
+        }
+    }
+}
