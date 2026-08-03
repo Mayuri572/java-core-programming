21490f083e039773ee3d42a60e99b70c37a28e03
Mayuri <jmayuri572@gmail.com>
2026-08-03 08:11:30 +0530

    Add examples of single-line and  multi-line comments

---
+++ src/Comments_03.java
@@ -0,0 +1,19 @@
+public class Comments_03 {
+    public static void main(String[] args) {
+        /*
+         * Program: Comments in Java
+         * Description:
+         * Demonstrates different types of comments used in Java.
+         */
+
+        // Single-line comment
+        System.out.println("This is a single-line comment example.");
+
+        /*
+         * Multi-line Comment
+         * Used to explain larger sections of code.
+         */
+        System.out.println("This is a multi-line comment example.");
+
+    }
+}
