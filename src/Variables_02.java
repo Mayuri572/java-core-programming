70499584d037fa7d38033e724dbbfec574f2b0d0
Mayuri <jmayuri572@gmail.com>
2026-08-03 08:07:55 +0530

    Add variables example covering primitive data types and string

---
+++ src/Variables_02.java
@@ -0,0 +1,34 @@
+public class Variables_02 {
+    public static void main(String[] args) {
+        /*
+         * Program: Variables in Java
+         * Description:
+         * Demonstrates declaration and initialization of variables
+         * using different primitive data types.
+         */
+
+        // Integer variable
+        int age = 20;
+
+        // Decimal number
+        double percentage = 94.1;
+
+        // single character
+        char grade = 'A';
+
+        // text/String
+        String name = "Mayuri";
+
+        // boolean value
+        boolean isStudent = true;
+
+        // Display Output
+        System.out.println("Name       : " + name);
+        System.out.println("Age        : " + age);
+        System.out.println("Percentage : " + percentage);
+        System.out.println("Grade      : " + grade);
+        System.out.println("Student    : " + isStudent);
+
+
+    }
+}
