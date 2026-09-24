09dba0f66d7d62882ad254ed28500dd94432fae4
Mayuri <jmayuri572@gmail.com>
2026-09-24 20:54:30 +0530

    Add variables example covering primitive data types

--- src/variablesanddatatypes/Variables.java
+++ src/variablesanddatatypes/Variables.java
@@ -1,8 +1,16 @@
 package variablesanddatatypes;
 
 public class Variables {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        int number = 10;
+        double percentage = 92.75;
+        String name = "Mayuri";
+        char grade = 'A';
 
+        System.out.println(number);
+        System.out.println(percentage);
+        System.out.println(name);
+        System.out.println(grade);
     }
-
 }
+
