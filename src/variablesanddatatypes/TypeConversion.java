d7cca459d13784c8617b7e7f93a02c9cc4f2c357
Mayuri <jmayuri572@gmail.com>
2026-09-24 22:17:45 +0530

    Add examples of type conversion

---
+++ src/variablesanddatatypes/TypeConversion.java
@@ -0,0 +1,16 @@
+package variablesanddatatypes;
+
+public class TypeConversion {
+    public static void main(String[] args){
+
+        /*int a = 20;
+        long b = a;
+        System.out.println(b);
+        */
+
+        long a = 20;
+        int b = a; // java: incompatible types: possible lossy conversion from long to int
+        System.out.println(b);
+
+    }
+}
