929c8fa0423ec0ea95e635e8f414b9fe801f8495
Mayuri <jmayuri572@gmail.com>
2026-09-24 20:55:31 +0530

    Add examples of single-line, multi-line, and JavaDoc comments

---
+++ src/variablesanddatatypes/Comments.java
@@ -0,0 +1,14 @@
+package variablesanddatatypes;
+
+public class Comments {
+    public static void main(String[] args){
+        // Single-line comment
+        System.out.println("This is a single-line comment example.");
+
+        /*
+         * Multi-line Comment
+         * Used to explain larger sections of code.
+         */
+        System.out.println("This is a multi-line comment example.");
+    }
+}
