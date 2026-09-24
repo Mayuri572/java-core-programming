e2329f33d1e570a33dd9339c22a7a351af751991
Mayuri <jmayuri572@gmail.com>
2026-09-24 22:25:00 +0530

    Add examples of type casting

---
+++ src/variablesanddatatypes/TypeCasting.java
@@ -0,0 +1,13 @@
+package variablesanddatatypes;
+
+public class TypeCasting {
+    public static void main(String[] args){
+        float a = 25.12f;
+        int b = (int) a;
+        System.out.println(b); // output: 25
+
+        char ch ='a';
+        int num = ch;
+        System.out.println(num); // output: 97
+    }
+}
