94fe711ec5061e6cf459cea2f4fc11f8072dffc1
Mayuri <jmayuri572@gmail.com>
2026-09-25 10:33:23 +0530

    Add logical operators examples covering AND, OR, and NOT operations

---
+++ src/operators/LogicalOperators.java
@@ -0,0 +1,15 @@
+package Operators;
+
+public class LogicalOperators {
+    public static void main(String[] args){
+
+        System.out.println( (3>2) && (5>3) );
+        System.out.println( (3<2) && (5>3) );
+
+        System.out.println( (3>2) || (5>3) );
+        System.out.println( (3<2) || (5>3) );
+
+        System.out.println( !(3>2) );
+        System.out.println( !(3<2) );
+    }
+}
