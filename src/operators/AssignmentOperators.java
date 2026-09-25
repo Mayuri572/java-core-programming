f86166d6fb96e5714cb027818dab9abec5af37c4
Mayuri <jmayuri572@gmail.com>
2026-09-25 10:39:50 +0530

    Add assignment operators examples covering compound assignment operations

---
+++ src/operators/AssignmentOperators.java
@@ -0,0 +1,24 @@
+package Operators;
+
+public class AssignmentOperators {
+    public static void main(String[] args){
+
+        int number = 5;
+        System.out.println("Initial Value : " + number);
+
+        number += 5;
+        System.out.println("After += 5 : " + number);
+
+        number -= 3;
+        System.out.println("After -= 3 : " + number);
+
+        number *= 2;
+        System.out.println("After *= 2 : " + number);
+
+        number /= 4;
+        System.out.println("After /= 4 : " + number);
+
+        number %= 3;
+        System.out.println("After %= 3 : " + number);
+    }
+}
