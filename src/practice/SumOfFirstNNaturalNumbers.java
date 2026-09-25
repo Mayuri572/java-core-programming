d1efe720936ad65738bf72da5fc951d3b363fe0b
Mayuri <jmayuri572@gmail.com>
2026-09-25 15:23:00 +0530

    Add practice problem to calculate the sum of first N natural numbers

---
+++ src/practice/SumOfFirstNNaturalNumbers.java
@@ -0,0 +1,23 @@
+package practice;
+
+import java.util.*;
+
+public class SumOfFirstNNaturalNumbers {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter N:  ");
+        int n = sc.nextInt();
+
+        int i = 1;
+        int sum = 0;
+
+        while(i <= n){
+            sum+= i;
+            i++;
+        }
+        System.out.println("Sum Of First" + n + " Natural Number: " + sum );
+        sc.close();
+    }
+}
